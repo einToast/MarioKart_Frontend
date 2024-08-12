60de59fe0dd3fa7a5634542c99642da71da4b89c
einToast <laurin2102@gmail.com>
2024-08-12 19:16:53 +0200

    Add Results functionality

--- src/pages/admin/Results.tsx
+++ src/pages/admin/Results.tsx
@@ -14,33 +14,38 @@ import {arrowBackOutline, arrowForwardOutline} from 'ionicons/icons';
 import {useEffect, useRef, useState} from "react";
 import axios from "axios";
 import "./Points.css"
+import {getTeams} from "../../util/api/RegistrationApi";
+import {getAllTeams} from "../../util/service/teamRegisterService";
+import {TeamReturnDTO} from "../../util/api/config/dto";
+import {checkToken, getTeamFinalRanked} from "../../util/service/adminService";
+import {useHistory} from "react-router";
 
 const results: React.FC<LoginProps> = (props: LoginProps) => {
-    const [teams, setTeam] = useState<Team[] | null>(null);
+    const [teams, setTeams] = useState<TeamReturnDTO[]>(null);
     const storageItem = localStorage.getItem('user');
     const user = JSON.parse(storageItem);
     const [userCharacter, setUserCharacter] = useState<string | null>(null);
-
-    const getTeamName = () => {
-        axios.get('http://localhost:3000/api/team/all')
-            .then ((response) => {
-                const responseData = response.data as Team[]
-                setTeam(responseData)
-            })
-            .catch((error) => {console.log(error)});
-    }
+    const history = useHistory();
 
     useEffect(() => {
-        getTeamName();
+        if (!checkToken()) {
+            window.location.assign('/admin/login');
+        }
+
+        const teamNames = getTeamFinalRanked();
+        teamNames.then((response) => {
+            console.log(response);
+            setTeams(response);
+        });
         setUserCharacter(user.character);
     },[])
 
     return (
         <IonPage>
             <IonContent fullscreen>
-                <div className={"back"}>
+                <div className={"back"} onClick={() => history.push('/admin/dashboard')}>
                     <IonIcon slot="end" icon={arrowBackOutline}></IonIcon>
-                    <a href={"/admin/dashboard"}>Zurück</a>
+                    <a>Zurück</a>
                 </div>
                 <h2>
                     <LinearGradient gradient={['to right', '#BFB5F2 ,#8752F9']}>
@@ -51,18 +56,16 @@ const results: React.FC<LoginProps> = (props: LoginProps) => {
                 <div className={"flexContainer"}>
                     {teams ? (
                         teams
-                            .sort((a, b) => b.punkte - a.punkte)
-                            .slice(0, 4)
                             .map(team => (
                                 <div key={team.id}
-                                     className={`teamContainer ${userCharacter === team.character ? 'userTeam' : ''}`}>
+                                     className={`teamContainer ${userCharacter === team.character.characterName ? 'userTeam' : ''}`}>
                                     <div className={"imageContainer"}>
-                                        <img src={`../resources/media/${team.character}.png`} alt={team.character}
+                                        <img src={`../resources/media/${team.character.characterName}.png`} alt={team.character.characterName}
                                              className={"iconTeam"}/>
                                     </div>
                                     <div>
-                                        <p>{team.name}</p>
-                                        <p className={"punkte"}>{team.punkte} Punkte</p>
+                                        <p>{team.teamName}</p>
+                                        <p className={"punkte"}>{team.finalPoints} Punkte</p>
                                     </div>
                                 </div>
                             ))
