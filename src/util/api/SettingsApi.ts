158fd7107f1a5aa6a9aeec91bcf63945b0cd2ff3
einToast <laurin2102@gmail.com>
2024-10-05 00:36:48 +0200

    Change api url to mariokart.eintoast.de - add constants for error and success color

---
+++ src/util/api/SettingsApi.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+import { API_BASE_URL } from './config/constants';
+import apiClient from "./config/apiClient";
+import {TournamentDTO} from "./config/dto";
+
+const BASE_URL = `${API_BASE_URL}/settings`;
+
+export const getSettings = async (): Promise<TournamentDTO> => {
+    try {
+        const response = await apiClient.get<TournamentDTO>(BASE_URL);
+        return response.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            if (error.response?.status === 404){
+                throw new Error('Einstellungen nicht gefunden');
+            } else if (error.response?.status === 401){
+                throw new Error('Nicht autorisierter Zugriff');
+            } else {
+                throw new Error('Einstellungen konnten nicht geladen werden');
+            }
+        }
+        throw error;
+    }
+};
+
+export const updateSettings = async (updateSettings: TournamentDTO): Promise<TournamentDTO> => {
+    try {
+        const response = await apiClient.put<TournamentDTO>(BASE_URL, updateSettings);
+        return response.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            if (error.response?.status === 404){
+                throw new Error('Einstellungen nicht gefunden');
+            } else if (error.response?.status === 401){
+                throw new Error('Nicht autorisierter Zugriff');
+            } else {
+                throw new Error('Einstellungen konnten nicht aktualisiert werden');
+            }
+        }
+        throw error;
+    }
+}
