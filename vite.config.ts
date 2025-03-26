61303c25a274830d1dbfea53a5b374da21621b71
einToast <laurin2102@gmail.com>
2025-03-27 00:03:06 +0800

    fix: error in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig, loadEnv } from 'vite'
+import legacy from '@vitejs/plugin-legacy';
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -29,13 +29,25 @@ export default defineConfig(({ mode }) => {
       setupFiles: './src_backup/setupTests.ts',
     },
     build: {
+      commonjsOptions: {
+        transformMixedEsModules: true
+      },
       rollupOptions: {
         output: {
           manualChunks: {
-            'vendor-ionic': ['@ionic/react', '@ionic/react-router'],
+            'vendor-base': ['react', 'react-dom'],
+            'vendor-ionic-core': ['@ionic/core'],
+            'vendor-ionic-react': ['@ionic/react'],
+            'vendor-routing': ['react-router', 'react-router-dom', '@ionic/react-router'],
             'vendor-charts': ['chart.js', 'react-chartjs-2'],
-            'vendor-react': ['react', 'react-dom', 'react-router', 'react-router-dom'],
-            'vendor-utils': ['axios', 'sockjs-client', '@stomp/stompjs', 'date-fns', 'js-cookie', 'jwt-decode'],
+            'vendor-utils': [
+              'axios',
+              'sockjs-client',
+              '@stomp/stompjs',
+              'date-fns',
+              'js-cookie',
+              'jwt-decode'
+            ]
           }
         }
       }
